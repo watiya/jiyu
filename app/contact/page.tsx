c010356704ea01d0555f1f7513b56632a05966ce
watiya <blingblingjiyu@gmail.com>
2023-11-15 00:14:18 +0900

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -13,9 +13,9 @@ const socials = [
 	// },
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "mailto:brainchild.jiyu@gmail.com",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "brainchild.jiyu@gmail.com",
 	},
 	{
 		icon: <Github size={20} />,
