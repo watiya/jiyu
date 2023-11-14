00c407307dc3b62d181baedcaeb9c0780ada3f60
watiya <blingblingjiyu@gmail.com>
2023-11-14 21:25:01 +0900

    Update article.tsx

--- app/projects/article.tsx
+++ app/projects/article.tsx
@@ -31,6 +31,9 @@ export const Article: React.FC<Props> = ({ project, views }) => {
 				<h2 className="z-20 text-xl font-medium duration-1000 lg:text-3xl text-zinc-200 group-hover:text-white font-display">
 					{project.title}
 				</h2>
+				<h2 className="z-20 text-xl font-medium duration-1000 lg:text-3xl text-zinc-200 group-hover:text-white font-display">
+					{project.title}
+				</h2>
 				<p className="z-20 mt-4 text-sm  duration-1000 text-zinc-400 group-hover:text-zinc-200">
 					{project.description}
 				</p>
