cf405fc0eef1b160e8ab9ef3a3696d03f43ddd19
watiya <blingblingjiyu@gmail.com>
2023-11-16 13:16:20 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -36,14 +36,7 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          I'm building{" "}
-          <Link
-            target="_blank"
-            href="https://unkey.dev"
-            className="underline duration-500 hover:text-zinc-300"
-          >
-            unkey.dev
-          </Link> to solve API authentication and authorization for developers.
+          I enjoy turning complex problems into simple, beautiful and intuitive designs. 
         </h2>
       </div>
     </div>
