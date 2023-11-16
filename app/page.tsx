efa0ce9ce497c4865add74c66a8c454a85a2dd40
watiya <blingblingjiyu@gmail.com>
2023-11-16 17:24:53 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Particles from "./components/particles";
 
 const navigation = [
   { name: "Projects", href: "/projects" },
-  { name: "About", href: "https://watiya.github.io/BrainChild/portfolio/index.html", target="_blank" rel="noopener noreferrer"},
+  { name: "About", href: "https://watiya.github.io/BrainChild/portfolio/index.html" target="_blank" rel="noopener noreferrer"},
   { name: "Portfolio", href: "https://watiya.github.io/BrainChild/portfolio3/index.html" },
   { name: "Contact", href: "/contact" },
 ];
